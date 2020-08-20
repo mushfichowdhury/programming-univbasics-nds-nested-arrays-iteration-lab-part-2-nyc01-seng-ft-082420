75a8d0f5875b04ae5774796561f0be070602d68b
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-20 18:31:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
   new_array = []
   while row_index < src.count do 
     element_index = 0 
-    min_value = src[row_index][element_index][0]
+    min_value = src[row_index][0]
     if src[row_index][element_index] < min_value
       min_value = src[row_index][element_index]
     end
